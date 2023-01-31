e03eba32d73ebebfd23e3f7086e68f744357dfe1
Longs Pemun Gotar <developerlongs@gmail.com>
2023-01-31 09:05:52 +0000

    changed endpoint and added env variable

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/joho/godotenv"
 )
 
 type AddressType struct {
@@ -14,14 +17,22 @@ type AddressType struct {
 }
 
 func main() {
-	http.HandleFunc("/address", func(w http.ResponseWriter, r *http.Request) {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	apiKey := os.Getenv("API_KEY")
+
+
+	http.HandleFunc("/check", func(w http.ResponseWriter, r *http.Request) {
 		address := r.URL.Query().Get("address")
 		if address == "" {
 			http.Error(w, "missing address parameter", http.StatusBadRequest)
 			return
 		}
 
-		client, err := ethclient.Dial("https://mainnet.infura.io/v3/sd")
+		client, err := ethclient.Dial("https://mainnet.infura.io/v3/" + apiKey)
 		if err != nil {
 			http.Error(w, "failed to connect to Ethereum client", http.StatusInternalServerError)
 			return
